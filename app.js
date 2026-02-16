5d2f4c358cb60a8be8473f9dc30b4bb62388d2d4
Shoyon Kumar <shoyonkumar143@gmail.com>
2026-02-16 16:34:08 +0600

    trending product section created and data also loaded form api

--- app.js
+++ app.js
@@ -1,1 +1,56 @@
-console.log("hey");
+const displayTrendngPdc = (products) => {
+  const container = document.getElementById("container");
+  container.innerHTML = "";
+
+  products.slice(0,3).forEach((product) => {
+    const div = document.createElement("div");
+    div.innerHTML = `
+     <div class="card bg-base-100 shadow-md h-full">
+  <figure class="h-56 flex items-center justify-center bg-gray-200 p-4">
+    <img
+      src="${product?.image}"
+      alt="${product?.title}"
+      class="h-full object-contain"
+    />
+  </figure>
+  <div class="card-body flex flex-col justify-between">
+    <div class="space-y-2">
+      <div class="flex justify-between items-center">
+        <h2 class="font-semibold bg-blue-100 text-blue-600 px-2 rounded-xl">
+          ${product?.category}
+        </h2>
+        <div class="text-xs"><i class="fa-solid fa-star text-yellow-300"></i>
+          ${product?.rating?.rate}/${product?.rating?.count}
+        </div>
+      </div>
+      <p class="text-sm">
+        ${product?.title}
+      </p>
+      <p class="font-bold text-lg">
+        $ ${product?.price}
+      </p>
+    </div>
+    <div class="flex justify-between mt-4">
+      <button class="btn btn-outline btn-sm">
+      <i class="fa-solid fa-eye"></i> Details
+      </button>
+      <button class="btn btn-primary btn-sm">
+        <i class="fa-solid fa-cart-shopping"></i> Add
+      </button>
+    </div>
+
+  </div>
+</div>
+    `;
+    container.append(div);
+  });
+};
+
+const getProducts = async () => {
+  const url = "https://fakestoreapi.com/products";
+  const res = await fetch(url);
+  const data = await res.json();
+  displayTrendngPdc(data);
+};
+
+getProducts();
